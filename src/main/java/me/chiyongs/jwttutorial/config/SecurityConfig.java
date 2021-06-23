84f0268063bb533e6e29918ad7f38089950a91ec
chiyongs <tlscldyd@gmail.com>
2021-06-23 19:08:22 +0900

    Security 설정, Data 설정

---
+++ src/main/java/me/chiyongs/jwttutorial/config/SecurityConfig.java
@@ -0,0 +1,30 @@
+package me.chiyongs.jwttutorial.config;
+
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+
+// 기본적인 웹 보안을 활성화하겠다는 의미의 어노테이션
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        web
+                .ignoring()
+                .antMatchers(
+                        "/h2-console/**"
+                        , "/favicon.ico"
+                );
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                .antMatchers("/api/hello").permitAll()
+                .anyRequest().authenticated();
+    }
+}
